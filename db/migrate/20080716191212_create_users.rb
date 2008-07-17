9590e8ca830738c992502b648aaf1e82fc260825
Stian Prestholdt <stian@prestholdt.no>
2008-07-18 01:32:33 +0200

    la til produkt spesifikasjon

--- db/migrate/20080716191212_create_users.rb
+++ db/migrate/20080716191212_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-      t.string   :login
+      t.string   :user_name
       t.string   :email
       t.string   :password
       t.string   :country
